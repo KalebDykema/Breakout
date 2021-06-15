c79af184d3940af574e77479e79b2c8a1b9e724d
Kaleb Dykema <dykemakaleb@gmail.com>
2021-06-15 00:20:53 -0500

    Heavily reorganized code

--- js/app.js
+++ js/app.js
@@ -32,62 +32,18 @@ for(let c = 0; c < brick.columns; c++){
 // Primary Rendering Function
 function draw(){
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  if(!ballShot) drawStart()
+  drawScore()
+  drawLives()
   drawMoon()
-  drawStart()
   drawBall()
   drawPaddle()
-  drawScore()
   drawBricks()
-  drawLives()
+
   collisionDetection()
-
-
   movementControls()
-
-  // // Checks to see if the ball is hitting a boundary. If so, bounces it off
-  // if(ball.x + ball.speedX > canvas.width-ball.radius || ball.x + ball.speedX < ball.radius ){
-  //   ball.speedX = -ball.speedX
-  //   ball.randomColor = getRandomColor()
-  // }
-  // if(ball.y + ball.speedY < ball.radius ){
-  //   ball.speedY = -ball.speedY
-  //   ball.randomColor = getRandomColor()
-  // }
-
-  // // If the balls hits the bottom wall, removes a life or ends the game if there's no lives left
-  // else if(ball.y + ball.speedY > canvas.height-ball.radius){
-  //   lives--
-  //   if(!lives){
-  //     alert('GAME OVER')
-  //     document.location.reload()
-  //     requestAnimationFrame(draw)
-  //   }
-  //   else{
-  //     ball.speedX = ball.baseSpeed
-  //     ball.speedY = -ball.baseSpeed
-  //     ball.x = paddle.x+(paddle.width/2)
-  //     ball.y = canvas.height-(paddle.height)
-  //     // paddle.x = (canvas.width-paddle.width)
-  //   }
-  // }
-
-  // Position Adjustments based on speed
-  if(ballShot){
-    ball.x += ball.speedX
-    ball.y += ball.speedY
-  }
-  // Sets the ball's position to above the paddle
-  else{
-    ball.x = paddle.x+(paddle.width/2)
-    ball.y = canvas.height-(paddle.height*2)
-  }
-
-  // If the moon goes off the screen, pushes it back to the other side
-  if(moon.x < 0 - moon.width){
-    moon.x = canvas.width+moon.width
-  }
-
-  moon.x -= moon.speedX
+  updatePositions()
 
   requestAnimationFrame(draw)
 }
---
+++ js/positionUpdating.js
@@ -0,0 +1,19 @@
+function updatePositions(){
+  // Ball position adjustments based on speed
+  if(ballShot){
+    ball.x += ball.speedX
+    ball.y += ball.speedY
+  }
+  // Sets the ball's position to above the paddle for the start of the game
+  else{
+    ball.x = paddle.x+(paddle.width/2)
+    ball.y = canvas.height-(paddle.height*2)
+  }
+
+  // If the moon goes off the screen, pushes it back to the other side
+  if(moon.x < 0 - moon.width){
+    moon.x = canvas.width+moon.width
+  }
+
+  moon.x -= moon.speedX
+}
